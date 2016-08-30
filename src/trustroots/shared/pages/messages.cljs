39b7a7b55ee203c9b956f7834a1ff31eaef184d7
Tarmo Aidantausta <tarmo.aidantausta@futurice.com>
2016-08-30 23:04:33 +0300

    List messages with list-view...
    ...wrapping ReactNative.ListView

--- src/trustroots/shared/pages/messages.cljs
+++ src/trustroots/shared/pages/messages.cljs
@@ -2,27 +2,31 @@
   (:require [reagent.core :as r :refer [atom]]
             [re-frame.core :refer [subscribe dispatch dispatch-sync]]
             [trustroots.shared.ui :as ui :refer
-             [view text image logo-img h1]]))
+             [view text image logo-img h1]]
+            [trustroots.shared.listview :refer
+             [list-view-with-subscription]]
+            [trustroots.helpers :refer [log info debug]]))
+
+(defn list-view-item [row]
+  (log row)
+  [view {:flex 1 :flex-direction "row"}
+   [text (get-in row [:message :excerpt])]])
 
 (defn messages-page [{style :style}]
-  (let [greeting (subscribe [:get-greeting])]
+  (let [messages (subscribe [:get-messages])]
     (fn []
-      [view {:style {:flex-direction "column"
-                     :margin 40
-                     :align-items "center"}}
-      [h1 "Messages"]
-      [image {:source ui/logo-img
-              :style  {:width 144 :height 144 :margin-bottom 30}}]
-
-      [ui/button { :text "Logout"
-                   :value "logout"
-                   :raised true
-                   :on-press #(dispatch [:logout])}]
-      [ui/button { :text "Refresh"
-                   :value "refresh"
-                   :raised true
-                   :on-press #(dispatch [:messages/fetch])}]
-
+      [view {:style { :flex-direction "column"
+                      :margin 40
+                      :align-items "center"}}
+       [list-view-with-subscription messages list-view-item "Messages"]
+       [ui/button { :text "Refresh"
+                    :value "refresh"
+                    :raised true
+                    :on-press #(dispatch [:messages/fetch])}]
+       [ui/button { :text "Logout"
+                    :value "logout"
+                    :raised true
+                    :on-press #(dispatch [:logout])}]
       ])))
 
 
